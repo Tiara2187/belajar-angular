a5a23c1064bbab9534ef58cdfcf415a436c785df
tiara2187 <tiaramuttianingtyas23@gmail.com>
2021-04-13 19:45:21 +0700

    perubahan pada app dan home componen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
 import { authConfig } from './sso.config';
+import { filter } from 'minimatch';
 
 @Component({
   selector: 'app-root',
@@ -11,22 +12,17 @@ export class AppComponent {
 
   constructor(private oauthService:OAuthService){
     this.configureSingleSignOn();
-
   }
 
   configureSingleSignOn(){
     this.oauthService.configure(authConfig);
     this.oauthService.tokenValidationHandler = new JwksValidationHandler();
-    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+    this.oauthService.loadDiscoveryDocumentAndLogin();
   }
 
-  login(){
-    this.oauthService.initImplicitFlow();
 
-  }
-
-  logout(){
-    this.oauthService.logOut();
-
+  get token(){
+    let claims:any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
   }
 }
